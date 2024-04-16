317afe5dd0d984ab57895943c0dbc67346ac63f6
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-04-17 00:30:57 +0530

    Setup Done, Router, Fonts, expo, Native Wind

---
+++ app/profile.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { Link } from 'expo-router'
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  )
+}
+
+export default Profile
+
+const styles = StyleSheet.create({})
