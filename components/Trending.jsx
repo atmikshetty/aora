a5af144f706b23f0f5b4b1b670de02df1caeb78d
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-04-21 11:28:12 +0530

    Videos section completed

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,13 +1,13 @@
-import {
-  View,
-  Text,
-  FlatList,
-  TouchableOpacity,
-  ImageBackground,
-  Image,
-} from "react-native";
-import * as Animatable from "react-native-animatable";
 import { useState } from "react";
+import { ResizeMode, Video } from "expo-av";
+import * as Animatable from "react-native-animatable";
+import {
+  FlatList,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
+
 import { icons } from "../constants";
 
 const zoomIn = {
@@ -30,6 +30,7 @@ const zoomOut = {
 
 const TrendingItem = ({ activeItem, item }) => {
   const [play, setPlay] = useState(false);
+
   return (
     <Animatable.View
       className="mr-5"
@@ -37,23 +38,36 @@ const TrendingItem = ({ activeItem, item }) => {
       duration={500}
     >
       {play ? (
-        <Text className="text-white">Playing</Text>
+        <Video
+          source={{ uri: item.video }}
+          className="w-52 h-72 rounded-[33px] mt-3 bg-white/10"
+          resizeMode={ResizeMode.CONTAIN}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={(status) => {
+            if (status.didJustFinish) {
+              setPlay(false);
+            }
+          }}
+        />
       ) : (
         <TouchableOpacity
-          className="relative justify-center items-center"
+          className="relative flex justify-center items-center"
           activeOpacity={0.7}
           onPress={() => setPlay(true)}
         >
           <ImageBackground
-            source={{ uri: item.thumbnail }}
-            className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40"
+            source={{
+              uri: item.thumbnail,
+            }}
+            className="w-52 h-72 rounded-[33px] my-5 overflow-hidden shadow-lg shadow-black/40"
             resizeMode="cover"
           />
 
-          <Image 
-          source={icons.play}
-          className="w-12 h-12 absolute"
-          resizeMode="contain"
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
           />
         </TouchableOpacity>
       )}
@@ -62,27 +76,27 @@ const TrendingItem = ({ activeItem, item }) => {
 };
 
 const Trending = ({ posts }) => {
-  const [activeItem, setActiveItem] = useState(posts[1]);
+  const [activeItem, setActiveItem] = useState(posts[0]);
 
-  const viewableItemsChanged = ({viewableItems}) => {
-    if(viewableItems.length > 0){
-      setActiveItem(viewableItems[0].key)
+  const viewableItemsChanged = ({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].key);
     }
-  }
+  };
 
   return (
     <FlatList
       data={posts}
+      horizontal
       keyExtractor={(item) => item.$id}
       renderItem={({ item }) => (
         <TrendingItem activeItem={activeItem} item={item} />
       )}
       onViewableItemsChanged={viewableItemsChanged}
       viewabilityConfig={{
-        itemVisiblePercentThreshold: 70
+        itemVisiblePercentThreshold: 70,
       }}
-      contentOffset={{x:170}}
-      horizontal
+      contentOffset={{ x: 170 }}
     />
   );
 };
--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,6 +1,7 @@
 import { View, Text, Image, Touchable, TouchableOpacity } from 'react-native'
 import { useState } from 'react'
 import { icons } from '../constants'
+import { Video, ResizeMode } from 'expo-av'
 
 const VideoCard = ({video: {title, thumbnail, video, creator: {username, avatar}}}) => {
 
@@ -38,7 +39,18 @@ const VideoCard = ({video: {title, thumbnail, video, creator: {username, avatar}
         </View>
 
     {play ? (
-        <Text className="text-white">Playing</Text>
+        <Video
+        source={{ uri: video }}
+        className="w-full h-60 rounded-xl mt-3"
+        resizeMode={ResizeMode.CONTAIN}
+        useNativeControls
+        shouldPlay
+        onPlaybackStatusUpdate={(status) => {
+          if (status.didJustFinish) {
+            setPlay(false);
+          }
+        }}
+      />
     ) : (
         <TouchableOpacity activeOpacity={0.7} onPress={() => setPlay(true)} className="w-full h-60 rounded-xl mt-3 relative justify-center items-center">
             <Image 
